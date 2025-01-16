3a87d6d0db78323ddd4ecd47c266f03d69abc780
kelseyz1229 <sunbo999999@hotmail.com>
2025-01-16 09:40:50 -0800

    cms

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "Fxb8Q~aHi~5xfAKnJ5jVziwzntsTFAcX5FONjbXv"
-    #CLIENT_SECRET = "BCS8Q~Y7Oy6oE0wPU78J9zEn4Gg4QXJZBuGmibbU"
+    #CLIENT_SECRET = "Fxb8Q~aHi~5xfAKnJ5jVziwzntsTFAcX5FONjbXv"
+    CLIENT_SECRET = "BCS8Q~Y7Oy6oE0wPU78J9zEn4Gg4QXJZBuGmibbU"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -42,8 +42,8 @@ class Config(object):
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
 
-    CLIENT_ID = "0ffdb341-d5c0-42b4-814b-e9b94fb76024"
-    #CLIENT_ID = "3494caa0-f78c-4fd1-9ebd-b98970887627"
+    #CLIENT_ID = "0ffdb341-d5c0-42b4-814b-e9b94fb76024"
+    CLIENT_ID = "3494caa0-f78c-4fd1-9ebd-b98970887627"
 
 
     #REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
