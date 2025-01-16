7fbed4eda6a2965a3c551311d04c345020388496
kelseyz1229 <sunbo999999@hotmail.com>
2025-01-15 17:51:13 -0800

    cms

--- config.py
+++ config.py
@@ -8,13 +8,17 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'image19'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'f48PPzbtI08VIDONxiDptUE3H/cN3VU1SPVjUgaY0Zr5fZnLE0++Cha13IG94Kig81AOYE8xh1Nk+AStPWmBeQ=='
+    #BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'image19'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'image19new'
+    #BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'f48PPzbtI08VIDONxiDptUE3H/cN3VU1SPVjUgaY0Zr5fZnLE0++Cha13IG94Kig81AOYE8xh1Nk+AStPWmBeQ=='
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'tygRIMIIsrR1dChzZFF+VBii7eSYi9VLhysRygYXPgmofnm9RzTmf0TgqJKPXgc9Gectvr5v/LQT+ASt/mtZgA=='
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'cms19.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms'
+    #SQL_SERVER = os.environ.get('SQL_SERVER') or 'cms19.database.windows.net'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'cmsnew.database.windows.net'
+    #SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cmsnew'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'cmsadmin'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'CMS4dmin'
     # Below URI may need some adjustments for driver version, baazuresed on your OS, if running locally
@@ -24,7 +28,8 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "Fxb8Q~aHi~5xfAKnJ5jVziwzntsTFAcX5FONjbXv"
+    #CLIENT_SECRET = "Fxb8Q~aHi~5xfAKnJ5jVziwzntsTFAcX5FONjbXv"
+    CLIENT_SECRET = "BCS8Q~Y7Oy6oE0wPU78J9zEn4Gg4QXJZBuGmibbU"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -37,11 +42,13 @@ class Config(object):
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
 
-    CLIENT_ID = "0ffdb341-d5c0-42b4-814b-e9b94fb76024"
+    #CLIENT_ID = "0ffdb341-d5c0-42b4-814b-e9b94fb76024"
+    CLIENT_ID = "3494caa0-f78c-4fd1-9ebd-b98970887627"
 
 
     #REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
-    REDIRECT_PATH = "/https://image19.azurewebsites.net/getAToken"
+    #REDIRECT_PATH = "/https://image19.azurewebsites.net/getAToken"
+    REDIRECT_PATH = "/https://localhost5555/getAToken"
 
 
 
